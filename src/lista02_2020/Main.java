71715c240f7dc4a6331efff5f4df35c1f7836ca4
julio <juliocswin@gmail.com>
2020-11-23 10:14:17 -0300

    segundo commit

--- src/lista02_2020/Main.java
+++ src/lista02_2020/Main.java
@@ -7,81 +7,93 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-
+		
 		Vetor v = new Vetor();
 		
 		int verificaValor;
+		int verificaTermino = 1;
 		
 		do {
+			do {
+				
+				System.out.println("Qual o tamanho do array gostaria? (Informe um valor maior que 0)");
+			    verificaValor = in.nextInt();
+				
+			} while(verificaValor <= 0);
 			
-			System.out.println("Qual o tamanho do array gostaria? (Informe um valor maior que 0)");
-		    verificaValor = in.nextInt();
+			if(verificaValor > 0) {
+				v.setArraySize(verificaValor);
+			}
+		
+			System.out.println("Array Gerado: ");
 			
-		} while(verificaValor <= 0);
+			for (int i = 0; i < v.getArraySize(); i++) {
 		
-		if(verificaValor > 0) {
-			v.setArraySize(verificaValor);
-		}
-	
-		System.out.println("Array Gerado: ");
-		
-		for (int i = 0; i < v.getArrayRandom().length; i++) {
-	
-			System.out.println(v.getArrayRandom()[i]);
+				System.out.println(v.getArrayRandom()[i]);
+				
+			}
 			
-		}
-		
-		System.out.println("");
-		System.out.println("Soma: " + v.retornaSoma());
-		
-		System.out.println("");
-		System.out.println("Media: " + v.retornaMedia());
-		
-		System.out.println("");
-		System.out.println("Moda: " +v.retornaModa());
-		
-		System.out.println("");
-		System.out.println("Mediana: " +v.retornaMediana());
-		
-		System.out.println("");
-		System.out.println("Variancia: " +v.retornaVariancia());
+			System.out.println("");
+			System.out.println("Soma: " + v.retornaSoma());
+			
+			System.out.println("");
+			System.out.println("Media: " + v.retornaMedia());
+			
+			System.out.println("");
+			System.out.println("Moda: " +v.retornaModa());
+			
+			System.out.println("");
+			System.out.println("Mediana: " +v.retornaMediana());
+			
+			System.out.println("");
+			System.out.println("Variancia: " +v.retornaVariancia());
 
-		System.out.println("");
-		System.out.println("Desvio Padrao: " +v.retornaDesvioPadrao());
-		
-		System.out.println("");
-		System.out.println("Maior Numero: " +v.retornaMaiorNumero());
-		
-		System.out.println("");
-		System.out.println("Menor Numero: " +v.retornaMenorNumero());
-	
-		System.out.println("");
-		System.out.println("Numeros Pares: ");
-		
-		for (int i = 0; i < v.getArrayRandom().length; i++) {
-	
-			System.out.println(v.retornaNumerosPares()[i]);
+			System.out.println("");
+			System.out.println("Desvio Padrao: " +v.retornaDesvioPadrao());
 			
-		}
-		
-		System.out.println("");
-		System.out.println("Numeros Impares: ");
-		
-		for (int i = 0; i < v.getArrayRandom().length; i++) {
-	
-			System.out.println(v.retornaNumerosImpares()[i]);
+			System.out.println("");
+			System.out.println("Maior Numero: " +v.retornaMaiorNumero());
 			
-		}
+			System.out.println("");
+			System.out.println("Menor Numero: " +v.retornaMenorNumero());
 		
-		System.out.println("");
-		System.out.println("Numeros Primos: ");
-		
-		for (int i = 0; i < v.getArrayRandom().length; i++) {
-	
-			System.out.println(v.retornaNumerosPrimos()[i]);
+			System.out.println("");
+			System.out.println("Numeros Pares: ");
 			
-		}
+			for (int i = 0; i < v.getArraySize(); i++) {
 		
+				System.out.println(v.retornaNumerosPares()[i]);
+				
+			}
+			
+			System.out.println("");
+			System.out.println("Numeros Impares: ");
+			
+			for (int i = 0; i < v.getArraySize(); i++) {
+		
+				System.out.println(v.retornaNumerosImpares()[i]);
+				
+			}
+			
+//			System.out.println("");
+//			System.out.println("Numeros Primos: ");
+//			
+//			for (int i = 0; i < v.getArraySize(); i++) {
+//		
+//				System.out.println(v.retornaNumerosPrimos()[i]);
+//				
+//			}
+			
+	
+			System.out.println("");
+			System.out.println("Gostaria de fazer outro calculo?");
+			System.out.println("Digite 1, para continuar");
+			System.out.println("Digite qualquer outra tecla para parar");
+			verificaTermino = in.nextInt();
+			
+		} while (verificaTermino == 1);
+		
+				
 		in.close();
 
 	}
